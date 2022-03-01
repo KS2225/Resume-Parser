8b30dd03da396fc591b1fca942d66880d47e6f56
Kshitija Supekar <74818465+KS2225@users.noreply.github.com>
2022-03-01 15:38:55 +0530

    Update New_LINUX_FINAL.py

--- New_LINUX_FINAL.py
+++ New_LINUX_FINAL.py
@@ -75,21 +75,17 @@ def load_image(image_file):
 	img = Image.open(image_file)
 	return img
 
-...
+st.subheader("Image")
+image_file = st.file_uploader("Upload Images", type=["png","jpg","jpeg"])
 
-if choice == "Image":
-		st.subheader("Image")
-		image_file = st.file_uploader("Upload Images", type=["png","jpg","jpeg"])
-
-		if image_file is not None:
+if image_file is not None:
 
 			  # To See details
-			  file_details = {"filename":image_file.name, "filetype":image_file.type,
-                              "filesize":image_file.size}
-			  st.write(file_details)
+	file_details = {"filename":image_file.name, "filetype":image_file.type,"filesize":image_file.size}
+	st.write(file_details)
 
               # To View Uploaded Image
-			  st.image(load_image(image_file),width=250)
+	st.image(load_image(image_file),width=250)
     
 
 button = st.sidebar.button("Select Folders and JD")
