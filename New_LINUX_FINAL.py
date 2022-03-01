90f073ab38ce0f1716a35d9efad2594b56c43995
Kshitija Supekar <74818465+KS2225@users.noreply.github.com>
2022-03-01 12:47:08 +0530

    Update New_LINUX_FINAL.py

--- New_LINUX_FINAL.py
+++ New_LINUX_FINAL.py
@@ -52,7 +52,23 @@ st.sidebar.write("---")
 #from tkinter import *
 #from tkinter import filedialog
 
+import wx
 
+app = wx.App()
+
+st.title('Folder Picker using wxPython')
+
+clicked = st.button('Click to select the folder', key = "FolderSelectionButton")
+
+if clicked:     
+    dlg_obj = wx.DirDialog (None, "Choose input directory", "",
+                        wx.DD_DEFAULT_STYLE | wx.DD_DIR_MUST_EXIST)
+                
+    if dlg_obj.ShowModal() == wx.ID_OK:
+        folder_path = dlg_obj.GetPath()
+
+    st.header('Selected Folder')
+    st.write(folder_path)
 
 
 
