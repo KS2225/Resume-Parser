75da075485174763f41862daf66dfa1d1e7a500d
Kshitija Supekar <74818465+KS2225@users.noreply.github.com>
2022-03-01 12:01:56 +0530

    Update New_LINUX_FINAL.py

--- New_LINUX_FINAL.py
+++ New_LINUX_FINAL.py
@@ -36,8 +36,8 @@ from cachetools import TTLCache
 
 st.title("Resume Screening")
 st.sidebar.image("https://upload.wikimedia.org/wikipedia/commons/thumb/7/73/Applus%2B_IDIADA_Logo.svg/1200px-Applus%2B_IDIADA_Logo.svg.png", width = 180)
-root = tk.Tk()
-root.withdraw()
+#root = tk.Tk()
+#root.withdraw()
 
 # Make folder picker dialog appear on top of other windows
 #root.wm_attributes('-topmost', 1)
